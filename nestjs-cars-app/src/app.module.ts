bc76fb35814c5532c72538fc77289acbbae04954
zen-ap <78710266+zen-ap@users.noreply.github.com>
2021-10-28 15:17:33 -0400

    Connecting front end to back end

--- nestjs-cars-app/src/app.module.ts
+++ nestjs-cars-app/src/app.module.ts
@@ -3,9 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ComponentsModule } from './components/components.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      playground: true,
+      debug: true,
+      autoSchemaFile: true,
+    }),
+    ComponentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- nestjs-cars-app/src/main.ts
+++ nestjs-cars-app/src/main.ts
@@ -1,9 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { BaseExceptionFilter, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
- 
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors();
+
   await app.listen(process.env.PORT || 9000);
 }
 bootstrap();
